27eacd32e9210c3911580e02cd0899bc0e28e9c2
Zachariah Lee <elezade@gmail.com>
2017-08-08 12:53:49 +0000

    added a user prompt and comments

--- initials.c
+++ initials.c
@@ -8,18 +8,22 @@
 int main (void)
 {
    
+    printf("Please enter your name: \n");
     //get users name and put it in a string
     string name = get_string();
     
     //int i = 0;
+    //get length of string 'name'
     for(int i = 0, n = strlen(name); i < n; i++)
     {
        if (i == 0)
        {
+           //if the letter is in 1st position make it uppercase
            printf("%c", toupper(name[i]));
        }
        else if(name[i - 1] == ' ')
        {
+           //if space before letter is blank make next letter upper
            printf("%c", toupper(name[i]));
        }
        
